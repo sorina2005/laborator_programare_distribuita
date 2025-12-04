5be45e9c736f316e6c39d8b6bf07b6499db87616
Sorina <ciobanusorina05@gmail.com>
2025-12-04 08:37:22 +0200

    changes on the files structure

---
+++ l1/ParSauImpar.py
@@ -0,0 +1,10 @@
+
+#luam inputul de la tastatura
+number = int(input("Numar care urmeaza a fi verificat: "))
+
+#calculam daca este numar par
+
+if number % 2 == 0:
+    print(f"{number} este un numar par.")
+else: print(f"{number} este un numar impar.")
+
